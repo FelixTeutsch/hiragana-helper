470a61a569323a7c78c7d46e1f34ff372a022a8f
Felix Teutsch <felix@teutsch.it>
2025-05-20 12:05:02 +0200

    Refactor hiragana-data.js to improve character hints for better learning clarity. Added detailed descriptions for each character's pronunciation and expanded the dataset with additional characters, including dakuon and combo characters. Marked sections for future additions of small characters and long vowels.

--- js/hiragana-data.js
+++ js/hiragana-data.js
@@ -1,30 +1,145 @@
 // Hiragana data for all character types
 export const hiraganaCharacters = [
-  // Gojūon
-  { char: 'あ', romaji: 'a', hint: 'ah' },
-  { char: 'い', romaji: 'i', hint: 'ee' },
-  { char: 'う', romaji: 'u', hint: 'oo' },
-  { char: 'え', romaji: 'e', hint: 'eh' },
-  { char: 'お', romaji: 'o', hint: 'oh' },
-  { char: 'か', romaji: 'ka', hint: 'cut' },
-  { char: 'き', romaji: 'ki', hint: 'key' },
-  { char: 'く', romaji: 'ku', hint: 'coup' },
-  { char: 'け', romaji: 'ke', hint: 'keg' },
-  { char: 'こ', romaji: 'ko', hint: 'coal' },
+  { char: 'あ', romaji: 'a', hint: 'Like "ah" in "father"' },
+  { char: 'い', romaji: 'i', hint: 'Like "ee" in "see"' },
+  { char: 'う', romaji: 'u', hint: 'Like "oo" in "moon"' },
+  { char: 'え', romaji: 'e', hint: 'Like "e" in "bed"' },
+  { char: 'お', romaji: 'o', hint: 'Like "o" in "go"' },
+
+  { char: 'か', romaji: 'ka', hint: 'Like "ca" in "car"' },
+  { char: 'き', romaji: 'ki', hint: 'Like "key"' },
+  { char: 'く', romaji: 'ku', hint: 'Like "coo" in "cool"' },
+  { char: 'け', romaji: 'ke', hint: 'Like "ke" in "kettle"' },
+  { char: 'こ', romaji: 'ko', hint: 'Like "co" in "coat"' },
+
+  { char: 'さ', romaji: 'sa', hint: 'Like "sa" in "saw"' },
+  { char: 'し', romaji: 'shi', hint: 'Like "she"' },
+  { char: 'す', romaji: 'su', hint: 'Like "sue"' },
+  { char: 'せ', romaji: 'se', hint: 'Like "se" in "set"' },
+  { char: 'そ', romaji: 'so', hint: 'Like "so" in "soap"' },
+
+  { char: 'た', romaji: 'ta', hint: 'Like "ta" in "tall"' },
+  { char: 'ち', romaji: 'chi', hint: 'Like "chee" in "cheese"' },
+  { char: 'つ', romaji: 'tsu', hint: 'Like "tsu" in "tsunami"' },
+  { char: 'て', romaji: 'te', hint: 'Like "te" in "tell"' },
+  { char: 'と', romaji: 'to', hint: 'Like "to" in "toe"' },
+
+  { char: 'な', romaji: 'na', hint: 'Like "na" in "nacho"' },
+  { char: 'に', romaji: 'ni', hint: 'Like "knee"' },
+  { char: 'ぬ', romaji: 'nu', hint: 'Like "new"' },
+  { char: 'ね', romaji: 'ne', hint: 'Like "ne" in "net"' },
+  { char: 'の', romaji: 'no', hint: 'Like "no" in "note"' },
+
+  { char: 'は', romaji: 'ha', hint: 'Like "ha" in "haha"' },
+  { char: 'ひ', romaji: 'hi', hint: 'Like "he" in "he"' },
+  { char: 'ふ', romaji: 'fu', hint: 'Like "who" but with lips not rounded' },
+  { char: 'へ', romaji: 'he', hint: 'Like "he" in "head"' },
+  { char: 'ほ', romaji: 'ho', hint: 'Like "ho" in "hope"' },
+
+  { char: 'ま', romaji: 'ma', hint: 'Like "ma" in "mama"' },
+  { char: 'み', romaji: 'mi', hint: 'Like "me" in "me"' },
+  { char: 'む', romaji: 'mu', hint: 'Like "moo" in "moon"' },
+  { char: 'め', romaji: 'me', hint: 'Like "me" in "met"' },
+  { char: 'も', romaji: 'mo', hint: 'Like "mo" in "more"' },
+
+  { char: 'や', romaji: 'ya', hint: 'Like "ya" in "yard"' },
+  { char: 'ゆ', romaji: 'yu', hint: 'Like "you"' },
+  { char: 'よ', romaji: 'yo', hint: 'Like "yo" in "yoga"' },
+
+  { char: 'ら', romaji: 'ra', hint: 'Like "ra" in "rap"' },
+  { char: 'り', romaji: 'ri', hint: 'Like "ree" in "reed"' },
+  { char: 'る', romaji: 'ru', hint: 'Like "rue"' },
+  { char: 'れ', romaji: 're', hint: 'Like "re" in "red"' },
+  { char: 'ろ', romaji: 'ro', hint: 'Like "ro" in "row"' },
+
+  { char: 'わ', romaji: 'wa', hint: 'Like "wa" in "water"' },
+  { char: 'を', romaji: 'o', hint: 'Like "o" in "go"' },
+  { char: 'ん', romaji: 'n', hint: 'Like "n" in "sin"' },
   // ... (add all gojūon)
   // Dakuon
   { char: 'が', romaji: 'ga', hint: 'gag' },
   { char: 'ぎ', romaji: 'gi', hint: 'geese' },
+  { char: 'ぐ', romaji: 'gu', hint: 'gum' },
+  { char: 'げ', romaji: 'ge', hint: 'get' },
+  { char: 'ご', romaji: 'go', hint: 'goat' },
+
+  { char: 'ざ', romaji: 'za', hint: 'zap' },
+  { char: 'じ', romaji: 'ji', hint: 'jig' },
+  { char: 'ず', romaji: 'zu', hint: 'zoo' },
+  { char: 'ぜ', romaji: 'ze', hint: 'zebra' },
+  { char: 'ぞ', romaji: 'zo', hint: 'zoo' },
+
+  { char: 'だ', romaji: 'da', hint: 'dare' },
+  { char: 'ぢ', romaji: 'ji', hint: 'jig' },
+  { char: 'づ', romaji: 'zu', hint: 'zoo' },
+  { char: 'で', romaji: 'de', hint: 'deer' },
+  { char: 'ど', romaji: 'do', hint: 'dog' },
+
+  { char: 'ば', romaji: 'ba', hint: 'baba' },
+  { char: 'び', romaji: 'bi', hint: 'bibi' },
+  { char: 'ぶ', romaji: 'bu', hint: 'bubu' },
+  { char: 'べ', romaji: 'be', hint: 'bebe' },
+  { char: 'ぼ', romaji: 'bo', hint: 'bobo' },
+
+  { char: 'ぱ', romaji: 'pa', hint: 'papa' },
+  { char: 'ぴ', romaji: 'pi', hint: 'pipi' },
+  { char: 'ぷ', romaji: 'pu', hint: 'pupu' },
+  { char: 'ぺ', romaji: 'pe', hint: 'pepe' },
+  { char: 'ぽ', romaji: 'po', hint: 'popo' },
+
+
+
   // ... (add all dakuon)
   // Combo characters
   { char: 'きゃ', romaji: 'kya', hint: 'key + ya' },
   { char: 'きゅ', romaji: 'kyu', hint: 'key + you' },
   { char: 'きょ', romaji: 'kyo', hint: 'key + yo' },
+
+  { char: 'ぎゃ', romaji: 'gya', hint: 'gag + ya' },
+  { char: 'ぎゅ', romaji: 'gyu', hint: 'gag + you' },
+  { char: 'ぎょ', romaji: 'gyo', hint: 'gag + yo' },
+
+  { char: 'しゃ', romaji: 'sha', hint: 'she + a' },
+  { char: 'しゅ', romaji: 'shu', hint: 'she + u' },
+  { char: 'しょ', romaji: 'sho', hint: 'she + o' },
+
+  { char: 'じゃ', romaji: 'ja', hint: 'jig + a' },
+  { char: 'じゅ', romaji: 'ju', hint: 'jig + u' },
+  { char: 'じょ', romaji: 'jo', hint: 'jig + o' },
+
+  { char: 'ちゃ', romaji: 'cha', hint: 'chi + a' },
+  { char: 'ちゅ', romaji: 'chu', hint: 'chi + u' },
+  { char: 'ちょ', romaji: 'cho', hint: 'chi + o' },
+
+  { char: 'にゃ', romaji: 'nya', hint: 'ny + a' },
+  { char: 'にゅ', romaji: 'nyu', hint: 'ny + u' },
+  { char: 'にょ', romaji: 'nyo', hint: 'ny + o' },
+
+  { char: 'ひゃ', romaji: 'hya', hint: 'hy + a' },
+  { char: 'ひゅ', romaji: 'hyu', hint: 'hy + u' },
+  { char: 'ひょ', romaji: 'hyo', hint: 'hy + o' },
+
+  { char: 'びゃ', romaji: 'bya', hint: 'bi + ya' },
+  { char: 'びゅ', romaji: 'byu', hint: 'bi + you' },
+  { char: 'びょ', romaji: 'byo', hint: 'bi + yo' },
+
+  { char: 'ぴゃ', romaji: 'pya', hint: 'pi + ya' },
+  { char: 'ぴゅ', romaji: 'pyu', hint: 'pi + you' },
+  { char: 'ぴょ', romaji: 'pyo', hint: 'pi + yo' },
+
+  { char: 'みゃ', romaji: 'mya', hint: 'mi + ya' },
+  { char: 'みゅ', romaji: 'myu', hint: 'mi + you' },
+  { char: 'みょ', romaji: 'myo', hint: 'mi + yo' },
+
+  { char: 'りゃ', romaji: 'rya', hint: 'ri + ya' },
+  { char: 'りゅ', romaji: 'ryu', hint: 'ri + you' },
+  { char: 'りょ', romaji: 'ryo', hint: 'ri + yo' },
+
   // ... (add all combos)
   // Small characters
-  { char: 'ぁ', romaji: 'xa', hint: 'small a' },
-  { char: 'ぃ', romaji: 'xi', hint: 'small i' },
+  // TODO: add small characters
   // ... (add all small chars)
   // Long vowels (represented by ー in katakana, but in hiragana, use double vowels)
+  // TODO: add long vowels
   // ... (no unique char, but can add examples if needed)
 ]; 
